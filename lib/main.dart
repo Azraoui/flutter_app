0b31baef0798b91767cd72ca203257b68330c53b
mustaphaouassit <mustaphaouassit60@gmail.com>
2021-07-07 10:01:30 +0100

    add.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Scroling App',
       theme: ThemeData(
           brightness: Brightness.dark,
           primaryColor: Colors.red,
@@ -32,43 +31,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _suggestion = <WordPair>[];
+  // start  ------------------------------------------
 
-  Widget _myList(int i) {
-    _suggestion.addAll(generateWordPairs().take(10));
-    String word = _suggestion[i].toString();
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Text(
-        word,
-        style: Theme.of(context).textTheme.headline1,
+  final List _suggestion = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = <WordPair>{};
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+        });
+      },
     );
   }
 
   Widget _buildSuggestions() {
-    return ListView(
-      children: [
-        _myList(0),
-        _myList(1),
-        _myList(2),
-        _myList(3),
-        _myList(4),
-        _myList(5),
-        _myList(0),
-        _myList(1),
-        _myList(2),
-        _myList(3),
-      ],
-    );
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, index) {
+          if (index.isOdd)
+            return const Divider(
+              color: Colors.yellow,
+            );
+          final i = index ~/ 2;
+          if (i >= _suggestion.length)
+            _suggestion.addAll(generateWordPairs().take(10));
+          return _buildRow(_suggestion[i]);
+        });
   }
 
+  // end ----------------------------------------------
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'App Bar',
+          'Startup Name Generator',
         ),
         backgroundColor: Theme.of(context).primaryColor,
       ),
